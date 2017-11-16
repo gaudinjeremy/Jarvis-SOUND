269c765afcfa6bdfe59bb90193c8f0e9c2e9dd98
Jérémy GAUDIN <gaudin.jeremy@gmail.com>
2017-11-16 23:59:05 +0100

    debug

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+jv_ia "addPlugin" "SOUND"
+jv_ia "addKey" "SOUND" "volume"
+jv_ia "addValue" "SOUND" "volume" "95"
+jv_ia "addKey" "SOUND" "micro"
+jv_ia "addValue" "SOUND" "micro" "90"
