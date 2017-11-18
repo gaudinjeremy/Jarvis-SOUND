7e0930094d2a45bfaf330bb24a0a2a0a2cee63e2
Jérémy GAUDIN <gaudin.jeremy@gmail.com>
2017-11-18 11:20:53 +0100

    Ajout de commentaires

--- functions.sh
+++ functions.sh
@@ -2,8 +2,8 @@
 
 #MODULE SOUND : Controle du volume de la carte son et du micro
 
-# $1 : carte son
-# $2 : controller SON
+# $1 : carte son (0,1)
+# $2 : controller SON (PCM, MIC)
 # $3 : valeur
 jv_set_sound(){
 
